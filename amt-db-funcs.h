b5372d2bce9158e0f9df6de9bae36c997e284bab
Simon Rowe <simon@wiremoons.com>
2017-02-04 09:09:19 +0000

    Correct function declaration to include ANSI format

--- amt-db-funcs.h
+++ amt-db-funcs.h
@@ -18,7 +18,7 @@ extern char *findme;
 
 int get_rec_count(void);		/* get current acronym record count */
 void check4DB(void);			/* ensure database is accessible */
-char *get_last_acronym();		/* get last acronym added to database */
+char *get_last_acronym(void);		/* get last acronym added to database */
 int do_acronym_search(char *findme);	/* search database for 'findme' string */
 int new_acronym(void);			/* add a new record enrty to the database */
 char *get_acro_src(void);		/* get a list of acronym sources */	
